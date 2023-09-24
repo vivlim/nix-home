319efbf9de8f56058ee1e0312b5ec982ec8d586e
Viv Lim <git@vvn.space>
2023-09-23 19:14:40 -0700

    add kubectl tab completions in bash

--- modules/kubernetes.nix
+++ modules/kubernetes.nix
@@ -1,5 +1,15 @@
 { pkgs, system, channels, bonusShellAliases, ... }: {
   home.packages = [
     pkgs.age
+    pkgs.jq
   ];
+
+  programs.bash = {
+    initExtra = ''
+      if command -v kubectl &> /dev/null
+      then
+        source <(kubectl completion bash)
+      fi
+    '';
+  };
 }
