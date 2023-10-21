434ea2bce9965e852dc88774c14a458d031c09dd
Viv Lim <git@vvn.space>
2023-10-21 01:37:42 -0700

    more improvements as i remove darwin-nix from macaroni-tome

--- modules/shell_mutable.nix
+++ modules/shell_mutable.nix
@@ -27,15 +27,29 @@
     $DRY_RUN_CMD ${addLineIfNotPresent file "FILE=${sourceTarget} && test -f $FILE && source $FILE"}
   '';
 in {
+  # Making sure nix is in the path, don't take it for granted on a mutable system.
+  home.packages = with pkgs; [
+    nix # Ensure that nix's bins will be in ~/.nix-profile/bin
+  ];
+  home.file.".nix_hm/nix_path" = {
+    text = ''
+    # Ensure nix bins are in $PATH
+    PATH=$HOME/.nix-profile/bin:$PATH
+    '';
+  };
+
+  # create the shell alias file ourselves.
   home.file.".nix_hm/shell_aliases" = {
     text = builtins.toString (lib.attrsets.mapAttrsToList (name: value: "alias ${name}=\"${value}\"\n") bonusShellAliases);
   };
 
+  # add activations steps that make sure we source the right files.
   home.activation = {
     bashSourceRc = addShellSourceFileIfPresent "~/.bashrc" "~/.nix_hm/bashrc";
     bashSourceShellAliases = addShellSourceFileIfPresent "~/.bashrc" "~/.nix_hm/shell_aliases";
     bashSourceHmSessionVars = addShellSourceFileIfPresent "~/.bashrc" "~/.nix-profile/etc/profile.d/hm-session-vars.sh";
     bashSourceProfile = addShellSourceFileIfPresent "~/.bash_profile" "~/.nix_hm/bash_profile";
+    bashSourceNixPath = addShellSourceFileIfPresent "~/.bash_profile" "~/.nix_hm/nix_path";
   } // (if lib.strings.hasSuffix "darwin" system then {
     # on mac, source .bashrc in .bash_profile since terminals create login shells
     bashSourceProfile = addShellSourceFileIfPresent "~/.bash_profile" "~/.bashrc";
