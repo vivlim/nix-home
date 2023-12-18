bff86c1385afdc3b18771627c6c9d6222296b94f
Viv Lim <git@vvn.space>
2023-12-18 02:55:09 -0800

    Add nixrb alias to xonsh as a shell script

--- modules/xonsh.nix
+++ modules/xonsh.nix
@@ -1,8 +1,16 @@
-{ pkgs, lib, system, channels, bonusShellAliases, ... }: {
+{ pkgs, lib, system, channels, bonusShellAliases, ... }: let
+
+  # ugh. need to factor this differently
+  nixrbScript = pkgs.writeShellScriptBin "nixrb" ''
+  ${bonusShellAliases.nixrb}
+  '';
+in {
   home.packages = [
     pkgs.xonsh-with-env
   ];
   home.file.".config/xonsh/rc.d/nix_hm.xsh".text = ''
+    from xonsh.built_ins import XSH
     xontrib load direnv
+    XSH.env["PATH"].append("${nixrbScript}/bin")
   '';
 }
