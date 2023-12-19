1d2feca956735286815a724d78559e7c0e66dce5
Viv Lim <git@vvn.space>
2023-12-19 02:33:23 -0800

    on mac, symlink lazygit's config
    on macos, lazygit looks in a different path for config.
    whether the $XDG_CONFIG_HOME env var is set properly
    is really hit-or-miss depending on how lazygit is started,
    so for simplicity just symlink it

--- mac.nix
+++ mac.nix
@@ -1,7 +1,18 @@
-{ pkgs, ... }:
+{ pkgs, home-manager, ... }:
 {
   home.packages = with pkgs; [ emacs28NativeComp bash ];
   home.sessionPath = [
     "$HOME/.emacs.d/bin" # doom emacs
   ];
+
+  home.activation = {
+    linkLazygitConfig = home-manager.lib.hm.dag.entryAfter ["writeBoundary"] ''
+      if [ -d ~/Library/Application\ Support/lazygit/config.yml ]; then
+        echo "~/Library/Application\ Support/lazygit/config.yml exists already"
+      else
+        echo "linking ~/Library/Application\ Support/lazygit/config.yml to ~/.config/lazygit/config.yml"
+        ln -s ~/.config/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
+      fi
+    '';
+  };
 }
