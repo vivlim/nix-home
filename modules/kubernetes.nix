bbed66fccb455f956ed6e44a29e69fe4e0bfd758
Viv Lim <git@vvn.space>
2023-12-18 23:21:03 -0800

    build lazykube in kubernetes module

--- modules/kubernetes.nix
+++ modules/kubernetes.nix
@@ -1,7 +1,20 @@
-{ pkgs, system, channels, bonusShellAliases, ... }: {
+{ pkgs, system, channels, bonusShellAliases, ... }: let
+lazykube = pkgs.buildGoModule rec {
+  pname = "lazykube";
+  version = "0.10.3";
+  src = pkgs.fetchFromGitHub {
+    owner = "TNK-Studio";
+    repo = "${pname}";
+    rev = "v${version}";
+    sha256 = "sha256-xfGzbjaH7dHeLWEaoRcYx5Gc3w4a4Kk1a1gG/EbXmSg=";
+  };
+  vendorHash = "sha256-3D+CElXHJv1WmBnO32hS4vW5VXXlNR3LwyGMtQJh3xs=";
+};
+in {
   home.packages = [
     pkgs.age
     pkgs.jq
+    lazykube
   ];
 
   programs.bash = {
