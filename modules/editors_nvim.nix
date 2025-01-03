f2cb0270c1204fcef8910c8e94f2c469652d0058
Viv Lim <vivlim@pm.me>
2025-01-03 14:41:56 -0800

    update nvim

--- modules/editors_nvim.nix
+++ modules/editors_nvim.nix
@@ -18,9 +18,9 @@ in {
     owner = "vivlim";
     repo = "vimfiles";
     rev =
-      "3458c0517d85d3957021f0413de7769c5f3d4fcc"; # #! ./_get_ref_commithash https://github.com/vivlim/vimfiles neovim
+      "be14824aa5cd998b7f17d70406a7fce14071acb5"; # #! ./_get_ref_commithash https://github.com/vivlim/vimfiles neovim
     sha256 =
-      "sha256-+9+QAMMTEbJH1ILah8M/YgFPMdZJT7dKCdM8aJ3Pvwk="; # #! ./_get_github_sha256 vivlim vimfiles neovim
+      "sha256-X5h3ftHCVjej/y4rTQIcY7ad6Sf7CF2Shzhz+84lDRw="; # #! ./_get_github_sha256 vivlim vimfiles neovim
   };
 
   home.sessionVariables = { EDITOR = "nvim"; };
